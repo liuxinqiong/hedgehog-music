d131f7342779deff91a595ce37082e497bb89dbd
liuxinqiong <372513646@qq.com>
2018-07-13 01:36:01 +0800

    router init

--- doc/start.md
+++ doc/start.md
@@ -2,3 +2,7 @@
 * 颜色规范，字体大小规范，通过变量提前定义
 * 开发方便
 * 换肤容易
+
+babel-runtime：辅助编译
+babel-polyfill：es6补丁
+fastclick：解决移动端点击300ms延迟
---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="m-header">
+        <div class="icon"></div>
+        <h1 class="text">Hedgehog Music</h1>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="stylus" scoped>
+
+</style>
