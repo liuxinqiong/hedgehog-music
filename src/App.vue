9f057149342d2846b96d4067480576327fc986b3
liuxinqiong <372513646@qq.com>
2018-07-13 00:31:38 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import "~common/stylus/variable"
+
+</style>
