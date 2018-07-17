d971f64f53d2f51b324a5a0da4b4f412666829bd
liuxinqiong <372513646@qq.com>
2018-07-17 22:49:06 +0800

    finished recommend page

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,34 +1,41 @@
 <template>
     <div class="recommend">
-        <div class="recommend-content">
-            <div v-if="recommends.length" class="slider-wrapper">
-                <Slider>
-                    <div v-for="(item, index) in recommends" :key="index">
-                        <a :href="item.linkUrl">
-                            <img :src="item.picUrl" alt="">
-                        </a>
-                    </div>
-                </Slider>
-            </div>
-            <div class="recommend-list">
-                <h1 class="list-title">热门歌单推荐</h1>
-                <ul>
-                    <li v-for="(item, index) in discList" :key="index" class="item">
-                        <div class="icon"><img :src="item.imgurl" alt="" width="60" height="60"></div>
-                        <div class="text">
-                            <h2 class="name" v-html="item.creator.name"></h2>
-                            <p class="desc" v-html="item.dissname"></p>
+        <scroll ref="scroll" class="recommend-content" :data="discList">
+            <div>
+                <div v-if="recommends.length" class="slider-wrapper">
+                    <Slider>
+                        <div v-for="(item, index) in recommends" :key="index">
+                            <a :href="item.linkUrl">
+                                <img class="needsclick" @load="loadImage" :src="item.picUrl" alt="">
+                            </a>
                         </div>
-                    </li>
-                </ul>
+                    </Slider>
+                </div>
+                <div class="recommend-list">
+                    <h1 class="list-title">热门歌单推荐</h1>
+                    <ul>
+                        <li v-for="(item, index) in discList" :key="index" class="item">
+                            <div class="icon"><img v-lazy="item.imgurl" alt="" width="60" height="60"></div>
+                            <div class="text">
+                                <h2 class="name" v-html="item.creator.name"></h2>
+                                <p class="desc" v-html="item.dissname"></p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
+            <div class="loading-container" v-show="!discList.length">
+                <loading></loading>
+            </div>
+        </scroll>
     </div>
 </template>
 <script>
 import { getRecommend, getDiscList } from '@/api/recommend'
 import { ERR_OK } from '@/api/config'
 import Slider from '@/base/slider/slider'
+import Scroll from '@/base/scroll/scroll'
+import Loading from '@/base/loading/loading'
 export default {
     data() {
         return {
@@ -54,10 +61,18 @@ export default {
                     this.discList = res.data.list
                 }
             })
+        },
+        loadImage() {
+            if(!this.checkLoaded) {
+                this.$refs.scroll.refresh()
+                this.checkLoaded = true
+            }  
         }
     },
     components: {
-        Slider
+        Slider,
+        Scroll,
+        Loading
     }
 }
 </script>
